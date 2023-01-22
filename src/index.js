cb5af700ae3acfd931042c3ca58a32c76f6dc6c3
unknown <myron.melnyk.kn.2021@lpnu.ua>
2023-01-22 17:58:08 +0200

    done task 1

--- src/index.js
+++ src/index.js
@@ -3,90 +3,53 @@ import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const BASE_URL = 'https://pixabay.com/api';
+
+let imgs_count = -1;
+let page = 1;
+let q = '';
+
 const refs = {
   gallery: document.querySelector('.gallery'),
-  images: document.querySelectorAll('.gallery__image'),
-  links: document.querySelectorAll('.gallery__link'),
+  input: document.querySelector('input'),
+  btn: document.querySelector('button'),
+  list: document.querySelector('.js-list'),
+  guard: document.querySelector('.js-guard'),
+  refreshGuard(observer) {
+    this.guard = document.querySelector('.js-guard');
+    observer.observe(this.guard);
+  },
 };
 
-const BASE_URL = 'https://pixabay.com/api';
-let imgs_count = -1;
-const params = new URLSearchParams({
+const URLparams = new URLSearchParams({
   key: '33023282-0c7c5ceb968646b6c20d323c8',
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
 });
 
-async function getCurrency(url) {
-  const promise = await axios.get(url);
-  const { data } = promise;
-  return data;
+const observerOptions = {
+  root: null,
+  rootMargin: '120px',
+  threshold: 0,
+  per_page: 40,
+};
+// --------------------------------------------------------- Observer ---------------------------------------------------------
+
+const observer = new IntersectionObserver(onLoad, observerOptions);
+
+function onLoad(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.remove();
+      page += 1;
+      const url = `${BASE_URL}?${URLparams}&q=${q}&page=${page}`;
+      render(url);
+    }
+  });
 }
 
-const q = 'cats';
-const url = `${BASE_URL}?${params}&q=${q}`;
-
-async function render(url) {
-  try {
-    const { hits: imgs } = await getCurrency(url);
-    const markup = creatMarkup(imgs);
-    refs.gallery.insertAdjacentHTML('beforeend', markup);
-  } catch (err) {
-    console.log(Notiflix.Notify.failure(err.message));
-  }
-}
-render(url);
-
-function creatMarkup(imgs) {
-  return imgs
-    .map(img => {
-      imgs_count += 1;
-      return `
-        <li class="gallery__item">
-          <a class="gallery__link" href="${img.largeImageURL}">
-            <div class="gallery__card" js-idx="${imgs_count}">
-               <div class="gallery__thumb">
-                    <img
-                    class="gallery__image"
-                    src="${img.webformatURL}"
-                    alt="${img.tags}"
-                    />
-                </div>
-                <ul class="info gallery__info">
-                    <li>
-                        <p class="info__item info__item--likes">
-                        <b
-                            >Likes
-                            <span class="info__num">${img.likes}</span>
-                        </b>
-                        </p>
-                    </li>
-                    <li>
-                        <p class="info__item info__item--views">
-                        <b>Views</b>
-                        <span class="info__num">${img.likes}</span>
-                        </p>
-                    </li>
-                    <li>
-                        <p class="info__item info__item--comments">
-                        <b>Comments</b>
-                        <span class="info__num">${img.likes}</span>
-                        </p>
-                    </li>
-                    <li>
-                        <p class="info__item info__item--downloads">
-                        <b>Downloads</b>
-                        <span class="info__num">${img.likes}</span>
-                        </p>
-                    </li>
-                </ul>
-            </div>
-          </a>
-        </li>`;
-    })
-    .join('');
-}
+// --------------------------------------------------------- LightBox ----------------------------------------------------------
 
 const lightbox = new SimpleLightbox('.gallery__link', {
   captionDelay: 250,
@@ -94,85 +57,98 @@ const lightbox = new SimpleLightbox('.gallery__link', {
 lightbox.on('show.simplelightbox', e => e.preventDefault());
 lightbox.on('error.simplelightbox', e => console.log(e));
 
-// --------------------------------------------------------- DILIMITER ---------------------------------------------------------
+// --------------------------------------------------------- Functions ---------------------------------------------------------
 
-// const list = document.querySelector('.js-list');
-// const guard = document.querySelector('.js-guard');
-// let page = 1;
-// const options = {
-//   root: null,
-//   rootMargin: '300px',
-//   threshold: 0,
-// };
+refs.btn.addEventListener('click', e => {
+  e.preventDefault();
+  refs.gallery.innerHTML = '';
+  q = refs.input.value.trim();
+  const url = `${BASE_URL}?${URLparams}&q=${q}`;
+  if (q) {
+    render(url);
+    smoothScroll();
+  }
+});
 
-// let observer = new IntersectionObserver(onLoad, options);
-// // document.addEventListener('scroll', onScroll)
-// // let test = 0
+async function render(url) {
+  try {
+    const { hits: imgs } = await getCurrency(url);
+    if (imgs.length == 0) throw new Error('💔 Nothing was found');
+    const markup = createMarkup(imgs);
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+    refs.refreshGuard(observer);
+    lightbox.refresh();
+  } catch (err) {
+    Notiflix.Notify.failure(err.message);
+  }
+}
 
-// // function onScroll() {
-// //     test += 1
-// //     console.log('scroll',test);
-// // }
+async function getCurrency(url) {
+  const promise = await axios.get(url);
+  const { data } = promise;
+  return data;
+}
 
-// // let observTest = 0
+function createMarkup(imgs) {
+  return imgs
+    .map(img => {
+      imgs_count += 1;
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${img.largeImageURL}">
+            <div class="gallery__card" js-idx="${imgs_count}">
+              <div class="gallery__thumb">
+                  <img
+                    class="gallery__image"
+                    src="${img.webformatURL}"
+                    alt="${img.tags}"
+                  />
+              </div>
+              <ul class="info gallery__info">
+                <li>
+                  <p class="info__item info__item--likes">
+                  <b
+                    >Likes
+                    <span class="info__num">${img.likes}</span>
+                  </b>
+                  </p>
+                </li>
+                <li>
+                  <p class="info__item info__item--views">
+                  <b>Views</b>
+                  <span class="info__num">${img.likes}</span>
+                  </p>
+                </li>
+                <li>
+                  <p class="info__item info__item--comments">
+                  <b>Comments</b>
+                  <span class="info__num">${img.likes}</span>
+                  </p>
+                </li>
+                <li>
+                  <p class="info__item info__item--downloads">
+                  <b>Downloads</b>
+                  <span class="info__num">${img.likes}</span>
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </a>
+        </li>`;
+    })
+    .concat('<div class="js-guard"></div>')
+    .join('');
+}
 
-// function onLoad(entries, observer) {
-//   // observTest += 1;
-//   // console.log('observer',observTest);
-//   entries.forEach(entry => {
-//     console.log(entry);
-//     if (entry.isIntersecting) {
-//       page += 1;
-//       lordOfTheRingsAPI(page)
-//         .then(data => {
-//           createMarkup(data.docs);
-//           if (data.page === data.pages) {
-//             observer.unobserve(guard);
-//           }
-//         })
-//         .catch(err => console.log(err));
-//     }
-//   });
-// }
+function smoothScroll() {
+  const firstCard = refs.gallery.firstElementChild;
 
-// function lordOfTheRingsAPI(page = 1) {
-//   const BASE_URL = 'https://the-one-api.dev/v2/character';
-//   const TOKEN = 'XJlq9OFMcHAy8pAQK7xj';
-//   const options = {
-//     method: 'GET',
-//     headers: {
-//       Authorization: `Bearer ${TOKEN}`,
-//     },
-//   };
+  if (firstCard) {
+    const { height: cardHeight } = firstCard.getBoundingClientRect();
 
-//   return fetch(
-//     `${BASE_URL}?limit=100&page=${page}&sort=name:asc`,
-//     options
-//   ).then(resp => {
-//     if (!resp.ok) {
-//       throw new Error(resp.statusText);
-//     }
-
-//     return resp.json();
-//   });
-// }
-
-// function createMarkup(arr) {
-//   const markup = arr
-//     .map(
-//       ({ name, race }) => `<li>
-//     <h2>${name}</h2>
-//     <h3>${race}</h3>
-// </li>`
-//     )
-//     .join('');
-
-//   list.insertAdjacentHTML('beforeend', markup);
-// }
-
-// lordOfTheRingsAPI()
-//   .then(data => {
-//     createMarkup(data.docs);
-//     observer.observe(guard);
-//   })
-//   .catch(err => console.log(err));
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  }
+}
